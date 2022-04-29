74134906b202b46fc1657b2be76e436a0889226f
AJ <velkymx@gmail.com>
2022-04-29 15:21:23 -0400

    Post schema

--- models/posts.js
+++ models/posts.js
@@ -1,11 +1,28 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const postsSchema = new Schema({
-    subject: String,
-    description: String,
+const postsSchema = new Schema(
+  {
+    title: String,
+    slug: String,
+    summary: String,
+    body: String,
+    feature_image: String,
+    tags: { type: [String], index: true },
+    settings: {
+      active: Boolean,
+      comment: Boolean,
+      featured: Boolean,
+      private: Boolean,
+      private_password: String,
+      redirect: String,
+      sticky: Boolean,
+    },
     account_id: String,
-    topic_id:String,
-}, { timestamps: true });
+    account_id: String,
+    publishedAt: Date,
+  },
+  { timestamps: true }
+);
 
-module.exports = Posts = mongoose.model('posts', postsSchema);
+module.exports = Posts = mongoose.model("posts", postsSchema);
