f8b56e86720502c4c4da7f787e8b66fc4235be78
AJ <velkymx@gmail.com>
2022-05-02 10:18:25 -0400

    API post routes

---
+++ controllers/posts.js
@@ -0,0 +1,29 @@
+const Posts = require("../models/posts");
+
+exports.index = function (req, res) {
+  Posts.find({}, function (err, response) {
+    if (err) return handleError(err);
+    res.json(response);
+  });
+};
+
+exports.create = function (req, res) {};
+
+exports.store = function (req, res) {};
+
+exports.show = function (req, res) {};
+
+exports.edit = function (req, res) {};
+
+exports.update = function (req, res) {};
+
+exports.destroy = function (req, res) {};
+
+// Verb	URI	Action	Route Name
+// GET	/photos/{photo}/comments	index	photos.comments.index
+// GET	/photos/{photo}/comments/create	create	photos.comments.create
+// POST	/photos/{photo}/comments	store	photos.comments.store
+// GET	/comments/{comment}	show	comments.show
+// GET	/comments/{comment}/edit	edit	comments.edit
+// PUT/PATCH	/comments/{comment}	update	comments.update
+// DELETE	/comments/{comment}	destroy	comments.destroy
--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const topics = require('../controllers/topics')
+const topics = require("../controllers/topics");
+const posts = require("../controllers/posts");
 
-router.get('/topics', topics.index);
+router.get("/topics", topics.index);
+router.get("/posts", posts.index);
 
-module.exports = router
+module.exports = router;
